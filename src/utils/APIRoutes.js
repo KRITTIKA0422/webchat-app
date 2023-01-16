bf4aeca5dde06e06d0e72f42c56a25d660f09cc4
Guvi-Assignment-day-21 <krittika489podder@gmail.com>
2023-01-16 23:45:12 +0530

    Added Vercel url

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,3 +1,3 @@
-export const host="http://localhost:4000";
+export const host="https://webchat-app-backend.vercel.app";
 export const registerRoute=`${host}/api/auth/register`;
 export const loginRoute=`${host}/api/auth/login`;
